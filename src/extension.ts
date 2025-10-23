8edf8d166bbdee14fbd2a14e15cd23866d46a97e
Alina <alinka._.0770pavlova@icloud.com>
2025-10-23 08:32:28 +0300

    privet open

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,12 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('Greeting Plugin is now active!');
 
   // Приветствие при запуске VS Code
-  vscode.window.showInformationMessage(
-      'Привет, пользователь! Добро пожаловать в VS Code!');
+  vscode.window.showInformationMessage('Привет! Добро пожаловать в VS Code!');
 
   // Команда, которую можно вызвать вручную
   const disposable =
       vscode.commands.registerCommand('greeting-plugin.helloWorld', () => {
-        vscode.window.showInformationMessage(
-            'Ты снова вызвал команду приветствия!');
+        vscode.window.showInformationMessage('Привет, хорошего тебе дня');
       });
 
   context.subscriptions.push(disposable);
